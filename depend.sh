9d79d2f3a1e203d2217a3bb5cc92a18cd3325e47
gkolpin <gkolpin@16ae21f4-ed28-0410-908c-9a1054b79e18>
2009-04-28 03:04:36 +0000

    - depend.sh fix to include path in target
    git-svn-id: svn+ssh://centos/home/fedora-home/external-backup/gkolpin/src/SVNROOT@364 16ae21f4-ed28-0410-908c-9a1054b79e18

--- depend.sh
+++ depend.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 DIR="$1"
+eval "LAST=\${$#}"
 shift 1
-gcc -iquote "$DIR" -MM -MG "$@" | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@'
+gcc -iquote "$DIR" -MT `echo "$LAST" | sed 's/\.c/\.o/'` -MM -MG "$@" | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@'
